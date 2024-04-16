de5e8c01173cc204d97d7baee6e8b27add694e15
NathanCCH <nathan.cch@outlook.com>
2024-04-16 13:00:53 -0600

    Update game.js

--- app/components/game.js
+++ app/components/game.js
@@ -1,8 +1,9 @@
+import Image from 'next/image'
 
 export default function Game({ thumb, title, salePrice, normalPrice, steamRatingPercent }) {
   return (
       <li className="flex items-center bg-pp-card p-2 rounded-md overflow-clip shadow-2xl hover:bg-pp-hover hover:ring">
-          <img src={thumb} alt={title} className="mx-2 rounded-md max-h-28 max-w-32 min-h-12 shadow-2xl" />
+          <Image src={thumb} alt={title} className="mx-2 rounded-md max-h-28 max-w-32 min-h-12 shadow-2xl" />
           <div className="mx-auto my-auto flex-grow text-right pr-2">
             <h2 className="text-sm font-black ">{title}</h2>
             <div className="mt-1">
