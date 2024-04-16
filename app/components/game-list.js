41c50bb2df1366bdcfe7de18fdaaf072074afae1
NathanCCH <nathan.cch@outlook.com>
2024-04-16 13:05:27 -0600

    Update game-list.js
    replace" ' " with &apos

--- app/components/game-list.js
+++ app/components/game-list.js
@@ -35,7 +35,7 @@ export default function GameList() {
   
   return (
     <div>
-      <h1 class="pt-5 mx-10 text-2xl text-gray-900 md:text-2xl lg:text-2xl dark:text-white"><mark class="px-2 text-white bg-blue-600 rounded dark:bg-blue-500">Today's</mark> deals</h1>
+      <h1 class="pt-5 mx-10 text-2xl text-gray-900 md:text-2xl lg:text-2xl dark:text-white"><mark class="px-2 text-white bg-blue-600 rounded dark:bg-blue-500">Today&apos;s</mark> deals</h1>
       <ul className="mx-10 pt-5 grid gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-full">
         {games.map((game) => (
           <Game
